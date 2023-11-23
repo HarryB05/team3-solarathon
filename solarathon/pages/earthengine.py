d7f5c20cba952ac8f82898f0a146c7fe584f5aeb
CodingCyanos <sigurdur.sigurdsson@cyanocapture.com>
2023-11-23 17:57:46 +0000

    added in

--- solarathon/pages/earthengine.py
+++ solarathon/pages/earthengine.py
@@ -39,6 +39,23 @@ class Map(geemap.Map):
         except:
             print("Could not generate sentinel")
 
+        # New example trial 
+        try:
+
+            roi =ee.Geometry.Polygon(
+                    [[[59.402645607468386, 36.721247854081604],
+                    [59.402645607468386, 6.293426486681035],
+                    [100.18389560746839, 6.293426486681035],
+                    [100.18389560746839, 36.721247854081604]]])
+
+            d_2 = ee.ImageCollection('MODIS/006/MYD11A1').filter(ee.Filter.date('2021-01-01', '2021-04-30')).select('LST_Day_1km')
+
+            clip=d_2.mean().clip(roi)     
+            band = {'min': 13000.0, 'max': 16500.0, 'palette': ['#00007b','#0e50f6', '05fce0', '1cfe07', 'f2fc03', '#feaf11', '#f46f28','#fe4709',],}
+            self.addLayer(clip, band,'LST',False,); 
+        except:
+            print("Temperature failure")
+
         # Set visualization parameters.
         vis_params = {
             'min': 0,
@@ -54,7 +71,7 @@ class Map(geemap.Map):
             'Landsat 7',
             False,
         )
-        self.addLayer(states, {}, "US States")
+        self.addLayer(states, {}, "US States",False,)
 
         
 
